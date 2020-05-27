5c3ed67852682cb57ff811dcbfb8a0fe57b25ff9
Patrick Miglar <miglar.patrick@gmail.com>
2020-05-27 10:27:32 +0200

    basic 1,2,3,4 done

--- basic.js
+++ basic.js
@@ -1,6 +1,26 @@
 function crystalGazer(job_title, location, partners_name, number_childrens) {
 
-console.log("You will be a"+job_title+"in"+location+"and married to"+partners_name+"with"+number_childrens)
+console.log("You will be a "+job_title+"in "+location+"and married to "+partners_name+"with "+number_childrens)
 }
 
 crystalGazer("Developer", "Vienna", "X", 2);
+
+
+function ageCalculator(byear) {
+  var year = new Date().getFullYear()
+  var age = (year - byear)
+  console.log("You are either "+age+" or "+(age-1))
+}
+
+ageCalculator(1990);
+
+
+function degToRad(degrees) {
+  var radians = degrees * Math.PI/180
+
+  console.log("radians "+radians)
+}
+
+degToRad(90);
+
+
