ee5a5f666f851f0cb1dc0a3c2d9897fd84cede3e
Patrick Miglar <miglar.patrick@gmail.com>
2020-05-27 09:40:19 +0200

    bla

---
+++ basic.js
@@ -0,0 +1,4 @@
+function crystalGazer (job_title, location, partners_name, number_childrens) {
+
+console.log("You will be a"+job_title+"in"+location+"and married to"+partners_name+"with"+number_childrens)
+}
