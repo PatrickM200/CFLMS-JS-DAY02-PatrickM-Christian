c76c877839e64bdfaba64539b0aa8eacc6ee34ea
Christian Gora <christian.gora@student.tuwien.ac.at>
2020-05-27 09:43:40 +0200

    basic - example

--- basic.js
+++ basic.js
@@ -1,4 +1,6 @@
-function crystalGazer (job_title, location, partners_name, number_childrens) {
+function crystalGazer(job_title, location, partners_name, number_childrens) {
 
 console.log("You will be a"+job_title+"in"+location+"and married to"+partners_name+"with"+number_childrens)
 }
+
+crystalGazer("Developer", "Vienna", "X", 2);
