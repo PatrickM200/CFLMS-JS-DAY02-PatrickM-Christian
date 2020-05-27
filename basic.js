a9987ab48202b3f1c0d3e69c1bac075db5b6145e
Christian Gora <christian.gora@student.tuwien.ac.at>
2020-05-27 11:01:04 +0200

    intermediate - finished

--- basic.js
+++ basic.js
@@ -23,4 +23,22 @@ function degToRad(degrees) {
 
 degToRad(90);
 
+function area_n_volume(width, height, depth) {
+	var area = width * height;
+	var volume = area * depth;
+	console.log("Area is: "+area+"\nVolume is: "+volume);
+}
 
+area_n_volume(1, 10, 2);
+
+var multipliers = [1,3,7,10,2];
+
+function product_of_array(array) {
+	var result = array[0];
+	for (var i = 1; i < array.length; i++) {
+		result*=array[i];
+	}
+	console.log(result);
+}
+
+product_of_array(multipliers);
